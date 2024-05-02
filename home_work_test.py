a7fb58b97e3b4c95b8fda81b51dd0d11f8603d7b
antoha-x <272acy@gmail.com>
2024-05-03 00:12:00 +0500

    Create new test - test_google_search_abra_cadabra_and_not_find_selene: search in google abra-cadabra and not find in result Selene

--- home_work_test.py
+++ home_work_test.py
@@ -5,3 +5,9 @@ def test_google_should_find_selene():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_google_search_abra_cadabra_and_not_find_selene():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('abra-cadabra').press_enter()
+    browser.element('[id="search"]').should(have.no.text('Selene - User-oriented Web UI browser tests in Python'))
